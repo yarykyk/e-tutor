c613968c893fbc818ab7ecbfba3d2ae269ac9d46
Yaroslay <yaroslavstrychak@gmail.com>
2025-04-12 22:57:13 +0300

    done second part(without adaptive)

--- scripts/burger_header.js
+++
@@ -1,7 +0,0 @@
-function burgerHeader(){
-    
-}
-document.querySelector('.header__burger-button').addEventListener('click', function(){
-    this.classList.toggle('is-active');
-    document.querySelector('.header__overlay').classList.toggle('is-active');
-})
