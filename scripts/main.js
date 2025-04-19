92840dcc2400dd83c5825a935186b848391f5ebc
Yaroslay <yaroslavstrychak@gmail.com>
2025-04-19 19:58:26 +0300

    added OurFeatureCorse block with burger adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
+// Header dropmenu
 document.querySelector('.header__burger-button').addEventListener('click', function(){
     this.classList.toggle('is-active');
     document.querySelector('.header__overlay').classList.toggle('is-active');
 })
+
+// Our Feature Corse
+document.querySelector('.OurFeatureCorse__burger-button').addEventListener('click', function(){
+    this.classList.toggle('is-active');
+    document.querySelector('.OurFeatureCorse__container').classList.toggle('is-active');
+})
+document.querySelector('.OurFeatureCorse__closeBlock').addEventListener('click', function () {
+    document.querySelector('.OurFeatureCorse__container').classList.toggle('is-active');
+    document.querySelector('.OurFeatureCorse__burger-button').classList.toggle('is-active');
+});
