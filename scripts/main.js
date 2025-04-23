621c89121025d1065a089c9b14b786852d4e43e6
Yaroslay <yaroslavstrychak@gmail.com>
2025-04-23 22:38:37 +0300

    Added TopInstructors part(no adaptive), some bug fixed

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,14 @@ document.querySelector('.OurFeatureCorse__closeBlock').addEventListener('click',
     document.querySelector('.OurFeatureCorse__container').classList.toggle('is-active');
     document.querySelector('.OurFeatureCorse__burger-button').classList.toggle('is-active');
 });
+
+// our top instructors
+
+document.querySelector('.TopInstructors__burger-button').addEventListener('click', function(){
+    this.classList.toggle('is-active');
+    document.querySelector('.TopInstructors__container').classList.toggle('is-active');
+})
+document.querySelector('.TopInstructors__closeBlock').addEventListener('click', function () {
+    document.querySelector('.TopInstructors__container').classList.toggle('is-active');
+    document.querySelector('.TopInstructors__burger-button').classList.toggle('is-active');
+});
