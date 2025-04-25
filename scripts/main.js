7172bf10720be20f03c37b41d02849c813cdd3c8
Yaroslay <yaroslavstrychak@gmail.com>
2025-04-25 23:11:55 +0300

    added top instructors block with adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,10 @@ document.querySelector('.OurFeatureCorse__closeBlock').addEventListener('click',
 document.querySelector('.TopInstructors__burger-button').addEventListener('click', function(){
     this.classList.toggle('is-active');
     document.querySelector('.TopInstructors__container').classList.toggle('is-active');
+    document.querySelector('.TopInstructors__top-instrucctors-container').classList.toggle('is-active');
 })
 document.querySelector('.TopInstructors__closeBlock').addEventListener('click', function () {
     document.querySelector('.TopInstructors__container').classList.toggle('is-active');
     document.querySelector('.TopInstructors__burger-button').classList.toggle('is-active');
+    document.querySelector('.TopInstructors__top-instrucctors-container').classList.toggle('is-active');
 });
